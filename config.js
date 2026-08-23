89fa1cc802907e6e35c5473be4af64b6abc1a8b2
medyslem93-lgtm <medyslem93@gmail.com>
2026-08-23 03:20:17 +0000

    Add shared Supabase configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.AGCH_CONFIG={
+  supabaseUrl:'https://pncjlbsflsgshmzgiiqu.supabase.co',
+  supabaseKey:'sb_publishable_fnl_v042_IqkcFPpP5oVLA_F_CrpRZX',
+  mediaBucket:'tournament-media'
+};
