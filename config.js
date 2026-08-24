445cd35d12db98b1dd9da3c9791585807af68244
medyslem93-lgtm <medyslem93@gmail.com>
2026-08-24 15:23:49 +0000

    Remove broken dynamic predictions loader

--- config.js
+++ config.js
@@ -3,16 +3,3 @@ window.AGCH_CONFIG={
   supabaseKey:'sb_publishable_fnl_v042_IqkcFPpP5oVLA_F_CrpRZX',
   mediaBucket:'tournament-media'
 };
-
-(() => {
-  const isAdmin=/\/admin(?:\/|$)/.test(location.pathname);
-  const css=document.createElement('link');
-  css.rel='stylesheet';css.href='predictions.css';document.head.appendChild(css);
-  if(isAdmin){
-    document.addEventListener('DOMContentLoaded',()=>{
-      const s=document.createElement('script');s.src='predictions.js';document.body.appendChild(s);
-    },{once:true});
-  }else{
-    document.write('<script src="predictions-v2.js"></script>');
-  }
-})();
