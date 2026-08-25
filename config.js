0603ded66223f9934acaadf1baeca30e79eff952
medyslem93-lgtm <medyslem93@gmail.com>
2026-08-25 18:42:22 +0000

    Load isolated match pane styles

--- config.js
+++ config.js
@@ -3,3 +3,10 @@ window.AGCH_CONFIG={
   supabaseKey:'sb_publishable_fnl_v042_IqkcFPpP5oVLA_F_CrpRZX',
   mediaBucket:'tournament-media'
 };
+
+(() => {
+  const css=document.createElement('link');
+  css.rel='stylesheet';
+  css.href='match-pane-fix.css?v=20260825-1';
+  document.head.appendChild(css);
+})();
